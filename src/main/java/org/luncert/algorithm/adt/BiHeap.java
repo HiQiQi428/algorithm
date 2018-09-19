d62e299949d32966446d35d6ac3a3ad26e8c91f8
HiQiQi428 <2725115515@qq.com>
2018-09-19 21:17:48 +0800

    .

--- src/main/java/org/luncert/algorithm/adt/BiHeap.java
+++ src/main/java/org/luncert/algorithm/adt/BiHeap.java
@@ -10,6 +10,7 @@ public class BiHeap<K, V> implements Adt<K, V> {
         V value;
     }
 
+    private final static int DEFAULT_OPACITY = 16;
     private Node<K,V>[] data;
     private int size;
     private boolean isMinHeap;
@@ -18,8 +19,13 @@ public class BiHeap<K, V> implements Adt<K, V> {
 
     }
 
+    @SuppressWarnings("unchecked")
     private void adjustCapacity() {
-        
+        if (size + 1 == data.length) {
+            Node<K,V>[] tmp = new Node[data.length + data.length >>> 1];
+            System.arraycopy(data, 0, tmp, 0, size);
+            data = tmp;
+        }
     }
 
     public void insert(K key, V value) {
---
+++ src/test/java/org/luncert/algorithm/adt/TestBiHeap.java
@@ -0,0 +1,15 @@
+package org.luncert.algorithm.adt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestBiHeap {
+    
+    @Test
+    public void test() {
+
+    }
+    
+}
